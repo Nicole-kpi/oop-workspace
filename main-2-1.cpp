db9932264652c69a9ca732c66231fb955519b815
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-03-12 16:57:25 +1030

    Add files via upload

---
+++ main-2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+extern void print_binary_str(string decimal_number);
+
+int main() {
+    print_binary_str("11"); 
+    return 0;
+}
