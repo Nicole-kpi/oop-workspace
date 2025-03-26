32c4be84f1ddc84aad5ea866fb9b4ee99f85621a
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-03-26 15:33:28 +1030

    comments

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -8,3 +8,22 @@ PersonList shallowCopyPersonList(PersonList pl) {
 
     return scpl;
 }
+
+PersonList createPersonList(int n){
+    PersonList pl;
+    
+    if (n <= 0){
+        pl.people = nullptr;
+        pl.numPeople = 0;
+
+        return pl;
+    }
+    pl.people = new Person[n];
+    pl.numPeople = n;
+
+    for (int i =0; i < n; i++){
+        pl.people[i].name = "Jane Doe";
+        pl.people[i].age = 1;
+    }
+    return pl;
+}
