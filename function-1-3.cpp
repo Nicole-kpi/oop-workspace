5384bc39a2b28aa0fdea08d1ff9b45f6461a2b3c
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-03-26 15:29:53 +1030

    comments

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -19,3 +19,22 @@ PersonList deepCopyPersonList(PersonList pl){
     }
     return dcpl;
 }
+
+PersonList createPersonList(int n){
+    PersonList pl;
+    
+    if (n <= 0){
+        pl.people = nullptr;
+        pl.numPeople = 0;
+
+        return pl;
+    }
+    pl.people = new Person[n];
+    pl.numPeople = n;
+
+    for (int i =0; i < n; i++){
+        pl.people[i].name = "Jane Doe";
+        pl.people[i].age = 1;
+    }
+    return pl;
+}
