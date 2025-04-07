1d53c2c4707f542a3c7f6b8d49eb936aadb12c34
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-04-08 01:29:19 +0930

    comment

--- Musician.h
+++ Musician.h
@@ -12,3 +12,14 @@ public:
     int get_experience();
 };
 #endif
+
+// #ifndef MUSICIAN_H
+// #define MUSICIAN_H
+// #include <string>
+// using namespace std;
+
+// class Musician {
+// private:
+//     string instrument;
+//     int experoence;
+// }
