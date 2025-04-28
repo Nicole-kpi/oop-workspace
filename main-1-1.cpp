00dba63cb358742993d084bfedf7dbb9e797c0b4
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-04-28 22:46:22 +0930

    comment

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -8,8 +8,8 @@ int main() {
     app1.turnOn();
     app2.turnOff();
 
-    std::cout << "App1 Power Rating: " << app1.getPowerRating() << ", Is On: " << app1.getIsOn() << std::endl;
-    std::cout << "App2 Power Rating: " << app2.getPowerRating() << ", Is On: " << app2.getIsOn() << std::endl;
+    std::cout << "App1 Power Rating: " << app1.get_powerRating() << ", Is On: " << app1.get_isOn() << std::endl;
+    std::cout << "App2 Power Rating: " << app2.get_powerRating() << ", Is On: " << app2.get_isOn() << std::endl;
     std::cout << "App1 Power Consumption: " << app1.getPowerConsumption() << std::endl;
 
     return 0;
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -5,14 +5,14 @@ int main() {
     Fridge fridge1;
     Fridge fridge2(200, 300.0);
 
-    fridge1.setVolume(150.0);
-    fridge1.setPowerRating(100);
+    fridge1.set_volume(150.0);
+    fridge1.set_powerRating(100);
 
     fridge1.turnOn();
     fridge2.turnOn();
 
-    std::cout << "Fridge1 Power Consumption: " << fridge1.getPowerConsumption() << std::endl;
-    std::cout << "Fridge2 Power Consumption: " << fridge2.getPowerConsumption() << std::endl;
+    std::cout << "Fridge1 Power Consumption: " << fridge1.get_powerConsumption() << std::endl;
+    std::cout << "Fridge2 Power Consumption: " << fridge2.get_powerConsumption() << std::endl;
 
     return 0;
 }
