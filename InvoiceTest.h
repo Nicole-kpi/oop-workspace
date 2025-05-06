c472925c4d1df1107b98b6bb4e5795f045d80bcf
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-05-06 22:06:05 +0930

    comments

---
+++ InvoiceTest.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "Invoice.h"
+
+class InvoiceTest {
+public:
+    void runTests() {
+        testAddServiceCost();
+        // Add other test methods here
+    }
+
+private:
+    void testAddServiceCost() {
+        {
+            Invoice invoice("ABCD");
+            invoice.addServiceCost(10);
+            if (invoice.getDollarsOwed() != 10) {
+                std::cout << "Test 1 failed!" << std::endl;
+            }
+        }
+
+        {
+            Invoice invoice("ABCD");
+            invoice.addServiceCost(1);
+            if (invoice.getDollarsOwed() != 1) {
+                std::cout << "Test 2 failed!" << std::endl;
+            }
+        }
+
+         {
+            Invoice invoice("ABCD");
+            invoice.addServiceCost(0);
+            if (invoice.getDollarsOwed() != 0) {
+                std::cout << "Test 3 failed!" << std::endl;
+            }
+        }
+    }
+
+     void testMultipleAdditions() {
+        Invoice invoice("ABCD");
+        invoice.addServiceCost(10);
+        invoice.addServiceCost(15);
+        if (invoice.getDollarsOwed() != 25)
+            std::cout << "Test 4 failed!" << std::endl;
+        else
+            std::cout << "Test 4 passed." << std::endl;
+    }
+};
+#endif
---
+++ UnitTest.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Addition.h"
+
+class UnitTest {
+public:
+    void runTests() {
+        testAddition();
+    }
+
+private:
+    void testAddition() {
+        Addition addition;
+
+        if (addition.add(1, 2) != 3) {
+            std::cout << "Test 1 failed!" << std::endl;
+        }
+        
+        if (addtion.add(0, 0) != 0) {
+            std::cout << "Test 2 failed!" << std::endl;
+        }
+
+        if (addition.add(-1, -1) != -2) {
+            std::cout << "Test 3 failed!" << std::endl;
+        }
+
+        if (addition.add(1, -2) != -1) {
+            std::cout << "Test 4 failed!" << std::endl;
+        }
+    }
+};
