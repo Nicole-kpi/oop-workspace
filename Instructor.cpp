aa455d904e4f2589457eaceeb675963204fe0a03
ZiYi Wang <ziyi.wang03@student.adelaide.edu.au>
2025-06-02 15:04:08 +0930

    Clear all files in main

--- Instructor.cpp
+++
@@ -1,8 +0,0 @@
-#include "Instructor.h"
-#include <iostream>
-using namespace std;
-
-Instructor::Instructor(const std::string& name) : Person(name) {}
-void Instructor::print() const {
-    cout << "Name of Instructor: " << name << endl;
-}
